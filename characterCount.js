e93b67eee6b1e9db83a3fcafd363252f894d02ff
Victoria <victoria.duggan@shopify.com>
2017-01-11 21:21:30 +0000

    updated example variable to match assignment

--- characterCount.js
+++ characterCount.js
@@ -1,5 +1,5 @@
 /*Write a function **countLetters** that can (return to) us us all the unique characters that exist in a string that is passed into the function. The function should also report back how many instances of each letter were found in the string.Our function countLetters needs to return an object that can represent the stats for the sentence it is given. */
-var example = "Loopy Lighthouse"
+var example = "lighthouse in the house"
 var letters = new Object;
 
 // function splits the string into characters and removes spaces. Uses letters Object to store value. Using ternary operator as the conditional to add 1 if the letter repeats.
